5cf19b608bea85e992d83454c76a82b354a5d2f5
Atahar Nur Pial <atp82232@gmail.com>
2022-02-01 19:23:00 +0600

    initial commit

---
+++ Data-validation/app.py
@@ -0,0 +1,50 @@
+import json
+# from pydantic import BaseModel
+from typing import Optional, List
+import pydantic
+
+class ISBN10FormatError(Exception):
+
+    def __init__(self, value: str, message: str) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(message)
+
+class Book(pydantic.BaseModel):
+    title: str
+    author: str
+    publisher: str
+    price: float
+    isbn_10: Optional[str]
+    isbn_13: Optional[str]
+    subtitle: Optional[str]
+
+    @pydantic.validator("isbn_10")
+    @classmethod
+    def isbn_10_valid(cls, value) -> None:
+        chars = [c for c in value if c in "0123456789Xx"]
+        if len(chars) != 10:
+            raise ISBN10FormatError(value=value, message="ISBN10 should be 10 digits.")
+
+        def char_to_int(char: str) -> int:
+            if char in "Xx":
+                return 10
+            return int(char)
+
+        if sum((10 - i) * char_to_int(x) for i, x in enumerate(chars)) % 11 != 0:
+            raise ISBN10FormatError(
+                value=value, message="ISBN10 digit sum should be divisible by 11."
+            )
+        return value
+
+def main() -> None:
+    with open("./data.json") as file:
+        data = json.load(file)
+        books: List[Book] = [Book(**item) for item in data]
+        # print(books[0])
+        print(books[0].dict(exclude={"isbn_10"}))
+
+        # print(data[0].title)
+
+if __name__ == "__main__":
+    main()
